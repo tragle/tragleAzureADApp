9cef5ff95273481fc682f7407a92263b0101055a
Tom Ragle <tragle@gmail.com>
2022-01-10 13:57:32 -0500

    env var

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const message = process.env.MESSAGE || 'your life';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'your life' });
+  res.render('index', { title: message });
 });
 
 module.exports = router;
